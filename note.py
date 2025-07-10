57742caceaf2cda47d4f832fd1aa6d41b5f76404
Brayan <br.cayun@duocuc.cl>
2025-07-09 21:24:06 -0400

    v3

--- note.py
+++ note.py
@@ -1,6 +1,18 @@
 def menu():
+    cant = 40
+    venta = 0
+    devolucion = 0
     print('''NOTEBOOKS MENÚ
 1. Cantidad de notebooks
 2. Venta de notebook
 3. Devolución de notebook
 4. Salir''')
+while True:
+    opc = int(input('Ingrese una opción: '))
+    if opc == 0:
+        print('Programa finalizado.')
+        break
+    elif opc == 1:
+        print(f'La cantidad de notebooks disponibles es: {cant}')
+        
+        
